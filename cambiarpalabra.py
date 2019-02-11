9d72b520104112aa40dfdda994d3a362733a62ff
Marlon Maravilla <46935734+mmaravilla28@users.noreply.github.com>
2019-02-10 23:04:59 -0600

    Add files via upload

---
+++ cambiarpalabra.py
@@ -0,0 +1,23 @@
+cantidad = input('\nDigite cuántas palabras tiene la lista: ')
+lista = []
+indice = 0
+cont = 0
+
+if int(cantidad) > 0:
+    while cont < int(cantidad):
+        cont += 1
+        lista.append(input('Digite la palabra {}: '.format(cont)))
+
+    print('\nLa lista creada es:', lista)
+
+    palabra = input('\nDigite la palabra a cambiar en la lista: ')
+    nuevapalabra = input('\nDigite la nueva palabra para agregar en la lista: ')
+
+    for x in lista:
+        if palabra == x:
+            lista[lista.index(x)] = nuevapalabra
+
+    print('\nLa nueva lista ahora es: ', lista)
+
+else:
+    print('\n¡Imposible crear la lista!')
